c5d88c4e7551d9fd6f97d53d2d5324f0fa3f7ba9
Hugo Bailey <lauren.bailey3@mail.bcu.ac.uk>
2020-06-17 13:07:53 +0100

    i dont know anymore i changed something?

---
+++ Temple Imperium/Assets/Scripts/Hugo's shite/healthPickUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthPickUp : MonoBehaviour
+{
+    [SerializeField]
+    int healthOnPickUp = 5;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<playerHealth>().RestoreHealth(healthOnPickUp);
+            Destroy(gameObject);
+        }
+    }
+}
