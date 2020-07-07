95722eb892c69530fc7f90cae9df4f14e87344a9
JoeAllen7 <Joseph.Allen@mail.bcu.ac.uk>
2020-07-07 14:42:42 +0100

    Reverb areas prefab
    Add to the map scene at coords 0,0,0 to apply adaptive reverb

---
+++ Temple Imperium Prototype/Assets/Scripts/Joe's code/ReverbAreas.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioReverbZone))]
+public class ReverbAreas : MonoBehaviour
+{
+    private AudioReverbZone reverbZone;
+
+    private void Start()
+    {
+        reverbZone = GetComponent<AudioReverbZone>();
+    }
+
+    public void TriggerReverb(string reverbAreaName)
+    {
+        Debug.Log("Triggering reverb for area: " + reverbAreaName);
+
+        switch (reverbAreaName)
+        {
+            case "LargeArea":
+                reverbZone.reverbPreset = AudioReverbPreset.Stoneroom;
+                break;
+            case "SmallArea":
+                reverbZone.reverbPreset = AudioReverbPreset.PaddedCell;
+                break;
+            default:
+                Debug.LogError("Unrecognised name for reverb area: " + reverbAreaName);
+                break;
+        }
+    }
+}
