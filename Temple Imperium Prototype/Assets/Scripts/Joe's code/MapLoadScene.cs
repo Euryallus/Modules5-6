3ab5d4989269206b2cc98095170c61e038670c0a
JoeAllen7 <Joseph.Allen@mail.bcu.ac.uk>
2020-07-21 12:06:31 +0100

    Map load scene

---
+++ Temple Imperium Prototype/Assets/Scripts/Joe's code/MapLoadScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MapLoadScene : MonoBehaviour
+{
+    public TextMeshProUGUI textLoading;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textLoading.text = "Loading (0%)";
+        StartCoroutine(LoadMapAsync());
+    }
+
+    private IEnumerator LoadMapAsync()
+    {
+        yield return null;
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync("MAP");
+
+        while (!loadOperation.isDone)
+        {
+            textLoading.text = "Loading (" + Mathf.RoundToInt(loadOperation.progress / 0.01f) + "%)";
+            yield return null;
+        }
+        yield return null;
+    }
+}
