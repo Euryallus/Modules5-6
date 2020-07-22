1b61f7bacb9d220021739e72e582b5e71cf645d7
JoeAllen7 <Joseph.Allen@mail.bcu.ac.uk>
2020-07-22 15:46:15 +0100

    UI click sounds

---
+++ Temple Imperium Prototype/Assets/Scripts/Joe's code/PlaySoundOnClick.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlaySoundOnClick : MonoBehaviour, IPointerClickHandler
+{
+    public string soundName = "Click";
+    public float volume = 1f;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            AudioManager.instance.PlaySoundEffect2D(soundName, volume);
+        }
+    }
+
+}
