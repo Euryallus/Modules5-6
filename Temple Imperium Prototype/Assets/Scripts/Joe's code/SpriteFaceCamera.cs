75ffc21507994566b84f1e7e0fb616a5a39fb17e
JoeAllen7 <Joseph.Allen@mail.bcu.ac.uk>
2020-08-03 20:05:46 +0100

    Added pickup sounds and text for health/ammo boxes

---
+++ Temple Imperium Prototype/Assets/Scripts/Joe's code/SpriteFaceCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+//------------------------------------------------------\\
+//  Forces sprites to always face towards the camera    \\
+//------------------------------------------------------\\
+//      Written by Joe for prototype phase              \\
+//------------------------------------------------------\\
+
+public class SpriteFaceCamera : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        //Force sprite to rotate towards the camera
+        transform.LookAt(Camera.main.transform, Vector3.up);
+    }
+}
