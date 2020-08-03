1893cc5953aa9c14a2b0a04e63f2eef6a7253936
JoeAllen7 <Joseph.Allen@mail.bcu.ac.uk>
2020-08-03 15:11:34 +0100

    Player can continue from controls screen with space to improve game flow

--- Temple Imperium Prototype/Assets/Scripts/Joe's code/MapLoadScene.cs
+++ Temple Imperium Prototype/Assets/Scripts/Joe's code/MapLoadScene.cs
@@ -14,17 +14,41 @@ public class MapLoadScene : MonoBehaviour
 {
     //Set in inspector:
     public TextMeshProUGUI textLoading; //The UI text used to display info/load percentage
+    public GameObject goLoadButton;     //The button that triggers the loading of the MAP scene
+    public GameObject goLoadIndicator;  //The animated loading indicator
+
+    private bool loading;       //Used to check if the next scene is currently being loaded
+    private float timeInScene;  //Used to keep track of the amount of time since the MapLoadScene was loaded
 
     // Start is called before the first frame update
     void Start()
     {
+        //Reset scene to default state
+        timeInScene = 0f;
+        goLoadButton.SetActive(true);
+        goLoadIndicator.SetActive(false);
+
         //Set starting text so the player knows to look at the controls
         textLoading.text = "Please familiarise yourself with the controls.";
+    }
+
+    private void Update()
+    {
+        //Increase time in scene by deltaTime so it counts up in realtime
+        timeInScene += Time.unscaledDeltaTime;
+
+        //Player can press space to start loading as an alternative to clicking the button
+        //  but only after being in the scene for 2 seconds, to prevent accidental skipping of the scene
+        if (Input.GetKeyDown(KeyCode.Space) && timeInScene > 2f)
+        {
+            ButtonReadyToLoad();
+        }
     }
 
     private IEnumerator LoadMapAsync()
     {
         //Start loading, reset load text
+        loading = true;
         textLoading.text = "Loading (0%)";
 
         //Wait for a short amount of time so the loading screen
@@ -46,6 +70,11 @@ public class MapLoadScene : MonoBehaviour
     //Starts loading the MAP scene on button load
     public void ButtonReadyToLoad()
     {
-        StartCoroutine(LoadMapAsync());
+        if (!loading)
+        {
+            StartCoroutine(LoadMapAsync());
+            goLoadButton.SetActive(false);
+            goLoadIndicator.SetActive(true);
+        }
     }
 }
